97db225fd93f22dddd079a76c2b15833edce315c
farahmahfouz <farahmahfouz11@gmail.com>
2025-03-01 01:06:20 +0200

    Feat/ Add footer and handle some ui

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BackgroundComponent } from '../../components/background/background.component';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BackgroundComponent],
+  imports: [BackgroundComponent, ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
